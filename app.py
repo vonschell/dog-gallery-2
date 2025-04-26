0c75ba7b599ee3c485f08d487a403fe40889f70d
vonschell <vonschell02@gmail.com>
2025-04-26 08:46:32 +0200

    Add number input validation and pass dog images, breed, and errors to template

--- app.py
+++ app.py
@@ -12,17 +12,22 @@ def check_breed(breed):
   return "/".join(breed.split("-"))
 
 @app.route("/", methods=["GET","POST"])
-def dog_image_gallery():
+def dog_images_gallery():
   errors = []
   if request.method == "POST":
     breed = request.form.get("breed")
+    number = request.form.get("number")
     if not breed:
       errors.append("Oops! Please choose a breed.")
-    if breed:
-      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/30")
+    if not number: 
+      errors.append("Oops! Please choose a number.")
+    if breed and number:
+      response = requests.get("https://dog.ceo/api/breed/" + check_breed(breed) + "/images/random/" + number)
       data = response.json()
       dog_images = data["message"]
-  return render_template("dogs.html")
+  return render_template("dogs.html", images=dog_images, breed=prettify_dog_breed(breed), errors=[])
+ return render_template("dogs.html", images=[], breed="", errors=errors)
+
 
 @app.route("/random", methods=["POST"])
 def get_random():
